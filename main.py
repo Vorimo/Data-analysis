5a817f4a7b01a7370ce1309d01c7ddf504f5cc2a
Ilya Ivanov <50019916+vorimo@users.noreply.github.com>
2022-04-14 11:52:45 +0300

    Files renaming

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import plotService
 
 if __name__ == '__main__':
     # pandas
-    excelService.gather_data_from_raw_extraction('extractions/Vpsk_68738292.csv', 'extractions/prepared/1.csv')
-    excelService.gather_data_from_raw_extraction('extractions/Vpsk_68738293.csv', 'extractions/prepared/2.csv')
+    output_march_file = 'extractions/prepared/03_2022.csv'
+    output_april_file = 'extractions/prepared/04_2022.csv'
+    excelService.gather_data_from_raw_extraction('extractions/03_2022.csv', output_march_file)
+    excelService.gather_data_from_raw_extraction('extractions/04_2022.csv', output_april_file)
     columns_to_extract = ['Amount', 'Transaction date']
-    extraction_march = excelService.read_from_excel('extractions/prepared/1.csv', columns_to_extract)
-    extraction_april = excelService.read_from_excel('extractions/prepared/2.csv', columns_to_extract)
+    extraction_march = excelService.read_from_excel(output_march_file, columns_to_extract)
+    extraction_april = excelService.read_from_excel(output_april_file, columns_to_extract)
     columns_to_show = [columns_to_extract[0]]
     x_label = 'date'
     y_label = 'amount'
