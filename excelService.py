3e87e5caf9b86c5513cf792f9f8a13c0338d9ddc
Ilya Ivanov <50019916+vorimo@users.noreply.github.com>
2022-04-14 17:20:55 +0300

    .csv files concatenation

--- excelService.py
+++ excelService.py
@@ -1,29 +1,36 @@
 import pandas as pd
 
 
-def gather_data_from_raw_extraction(extractions_file_name, save_path: str):
-    raw_extraction_df = pd.read_csv(extractions_file_name, encoding='cp1251', sep=';', engine='python',
-                                    usecols=[0, 1, 2, 8])
-    # drop empty values
-    prepared_extraction_df = raw_extraction_df.dropna()
-    # columns rename
-    prepared_extraction_df = prepared_extraction_df.rename(columns={
-        'Дата транзакции': 'Transaction date',
-        'Операция': 'Operation',
-        'Сумма': 'Amount',
-        'Валюта': 'Currency',
-        'Категория операции': 'Category'
-    })
-    # trim all strings
-    prepared_extraction_df = prepared_extraction_df.applymap(lambda x: x.strip())
-    # sort by date desc
-    prepared_extraction_df = prepared_extraction_df.sort_values(by='Transaction date', ascending=False)
-    # sanitize numeric input
-    prepared_extraction_df['Amount'] = prepared_extraction_df['Amount'].apply(lambda x: sanitize_numeric_input(x))
+def gather_data_from_raw_extractions(extractions_file_names, save_path: str):
+    full_data = pd.DataFrame()
+    for extraction_file_name in extractions_file_names:
+        raw_extraction_df = pd.read_csv(extraction_file_name, encoding='cp1251', sep=';', engine='python',
+                                        usecols=[0, 1, 2, 8])
+        # drop empty values
+        prepared_extraction_df = raw_extraction_df.dropna()
+        # columns rename
+        prepared_extraction_df = prepared_extraction_df.rename(columns={
+            'Дата транзакции': 'Transaction date',
+            'Операция': 'Operation',
+            'Сумма': 'Amount',
+            'Валюта': 'Currency',
+            'Категория операции': 'Category'
+        })
+        # trim all strings
+        prepared_extraction_df = prepared_extraction_df.applymap(lambda x: x.strip())
+        # sort by date desc
+        prepared_extraction_df = prepared_extraction_df.sort_values(by='Transaction date', ascending=False)
+        # sanitize numeric input
+        prepared_extraction_df['Amount'] = prepared_extraction_df['Amount'].apply(lambda x: sanitize_numeric_input(x))
+        full_data = pd.concat([full_data, prepared_extraction_df])
+    save_df_to_csv(full_data, save_path)
+
+
+def save_df_to_csv(df, save_path: str):
     # open a file for writing
     prepared_extraction_file = open(save_path, "w", encoding="utf-8")
     # convert df to .csv with no df index
-    prepared_extraction_file.write(prepared_extraction_df.to_csv(sep=';', line_terminator='\n', index=False))
+    prepared_extraction_file.write(df.to_csv(sep=';', line_terminator='\n', index=False))
     prepared_extraction_file.close()
 
 
--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import plotService
 
 if __name__ == '__main__':
     # pandas
-    output_march_file = 'extractions/prepared/03_2022.csv'
-    output_april_file = 'extractions/prepared/04_2022.csv'
-    excelService.gather_data_from_raw_extraction('extractions/03_2022.csv', output_march_file)
-    excelService.gather_data_from_raw_extraction('extractions/04_2022.csv', output_april_file)
+    output_file = 'extractions/prepared/full_data.csv'
+    excelService.gather_data_from_raw_extractions([
+        'extractions/raw/03_2022.csv',
+        'extractions/raw/04_2022.csv'
+    ], output_file)
     columns_to_extract = ['Amount', 'Transaction date']
-    extraction_march = excelService.read_from_excel(output_march_file, columns_to_extract)
-    extraction_april = excelService.read_from_excel(output_april_file, columns_to_extract)
+    full_extraction = excelService.read_from_excel(output_file, columns_to_extract)
     columns_to_show = [columns_to_extract[0]]
     x_label = 'date'
     y_label = 'amount'
     title = 'Money spending data'
-    plotService.plot_df(extraction_march, columns_to_show, x_label, y_label, title, 'March')
-    plotService.plot_df(extraction_april, columns_to_show, x_label, y_label, title, 'April')
-    plotService.save_plot_and_show('extractions/plots/1.png')
+    plotService.plot_df(full_extraction, columns_to_show, x_label, y_label, title, 'BYN')
+    plotService.save_plot_and_show('extractions/plots/full_data.png')
